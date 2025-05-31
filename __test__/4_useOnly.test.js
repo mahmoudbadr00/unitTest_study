5b7adfb7c62fb908c66998d893a2347d984e0dfe
mahmoud badr <mahmoud.tamer.badr2000@gmail.com>
2025-05-31 23:51:25 +0300

    fix

---
+++ __test__/4_useOnly.test.js
@@ -0,0 +1,20 @@
+const theSum = require("../sum");
+
+// ONLY
+// test.only(name, fn, timeout) or describe.only(name, fn, timeout)
+// it tests only what i write only on it
+// rest of tests , describes are skipped if not called with .only() or .skip()
+
+describe("sum function", () => {
+  describe.only("when input is only one number", () => {
+    test.only("sum function", () => {
+      expect(theSum(1)).toBe(1);
+    });
+    test("sum function with negative numbers", () => {
+      expect(theSum(-1)).toBe(-1);
+    });
+    test.skip("sum function with zero", () => {
+      expect(theSum(0)).toBe(0);
+    });
+  });
+});
---
+++ methods.js
@@ -0,0 +1,20 @@
+/*
+beforeAll(fn, timeout)  ==> beforeAll tests
+beforeEach(fn, timeout) ==> beforeEach tests
+*/
+
+
+/*beforeAl1(()=>{
+//Connect  To.Database
+//Empty  Testing .Table
+//Add.Dummy  Data . For. Testing
+//Prepare.Everything .Before.Testing
+);
+*/
+
+/*afterAl1(()=> {
+//.Clean Database
+.//.Clean  Cache
+.//.Clean  Testing .Table
+});
+*/
